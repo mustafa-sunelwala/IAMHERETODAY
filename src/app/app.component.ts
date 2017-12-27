b13c7642d0cb3bdd650051a1645167196d94a871
Mustafa Sunelwala <mustafa.sunelwala@Perficient.com>
2017-12-27 13:26:56 +0530

    changes in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 /**
  * Angular 2 decorators and services
  */
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { environment } from 'environments/environment';
+import { Component, OnInit } from '@angular/core';
 import { AppState } from './app.service';
 
 /**
@@ -11,33 +10,17 @@ import { AppState } from './app.service';
  */
 @Component({
   selector: 'app',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: [
     './app.component.css'
   ],
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  public name = 'Angular Starter';
-  public tipe = 'assets/img/tipe.png';
-  public twitter = 'https://twitter.com/gdi2290';
-  public url = 'https://tipe.io';
-  public showDevModule: boolean = environment.showDevModule;
 
-  constructor(
-    public appState: AppState
-  ) {}
+  constructor(public appState: AppState) {}
 
   public ngOnInit() {
     console.log('Initial App State', this.appState.state);
   }
 
 }
-
-/**
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- */
