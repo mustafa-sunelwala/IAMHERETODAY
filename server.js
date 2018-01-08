2dcd110bdaf937cc0ab646c4995e0c060e0076f6
Mustafa Sunelwala <mustafa.sunelwala@Perficient.com>
2018-01-08 12:48:07 +0530

    implemented cors plugin in server.

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
 // Get our API routes
 const api = require('./server/routes/api');
 
 const app = express();
+
+app.use(cors());
 
 // Parsers for POST data
 app.use(bodyParser.json());
